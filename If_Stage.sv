cae886a1f0462472128d83dbd5c3a02bbf1a9353
Anurag Singh <anuragsi003@gmail.com>
2026-01-06 16:17:30 +0100

    Update If_Stage.sv

--- If_Stage.sv
+++ If_Stage.sv
@@ -2,20 +2,21 @@
 ///Package Importt/
 `include "cpu_pkg.sv"
 module If_stage (
-                input logic clk,
-  				input logic rst,
+                input logic Clk,
+  				input logic Rst_n,
+  				input logic Start, // Synchronous start Button-- Enable
 
           ///Instruction SRAM wr Interface/// For loading SRAM 
-               output logic imem_en ,
- 			   output logic [INST_ADDR_WIDTH -1:0] imem_addr,
- 			   input logic [INST_DATA_WIDTH-1:0] imem_data,
+               output logic Imem_En ,
+			   output logic [INST_ADDR_WIDTH -1:0] Imem_Addr,
+			   input logic [INST_DATA_WIDTH-1:0] Imem_Data,
          /// EX Stage Interface
-				input logic Ex_isBranchTaken,
+				input logic Ex_IsBranchTaken,
 	            input logic [31:0] Ex_BranchPC,
 	
 		 ///OF Stage Interface
 				//IF Stage Output
-			    output logic If_id_valid,
+			    output logic If_Valid,
 				input If_Of_t If_Payld,
 
 ) ;
@@ -30,11 +31,11 @@ module If_stage (
 	begin 
 	   pc_nxt = Ex_isBranchTaken ? Ex_BranchPC : pc + 32'd4 ; // /// Incrementing by 4 bytes for the next instruction
 	    //---------Instruction SRAM Controls-------------//
-       imem_en= start ; // always available /// if their is address change
-       imem_addr =  pc[INST_ADDR_WIDTH+1 : 2];  // 	•	PC[1:0] → byte offset (always 00) •	PC[2] → selects instruction 1
-       instr = imem_data ; 
+       Imem_En= start ; // always available /// if their is address change
+       Imem_Addr =  pc[INST_ADDR_WIDTH+1 : 2];  // 	•	PC[1:0] → byte offset (always 00) •	PC[2] → selects instruction 1
+       instr = Imem_Data ; 
 	end
-  //// Either PC points to same addrwss to move to Next , Depending on Enable.
+//// Either PC points to same addrwss to move to Next , Depending on Enable.
   always@(negedge clk, negedge rst)
     if(!rst) pc <= '0;
   else if(start)  pc <= pc_nxt ;
@@ -42,18 +43,17 @@ module If_stage (
 
 // Pipeline Interface 
 logic [31:0] pc_q;     // delayed PC to match imem_data
-If_Id_t if_id_payld_nxt, if_id_payld;
-logic  If_id_valid;
-
+If_Of_t if_payld_nxt;
+	
 // Delay PC to align with synchronous IMEM output
 always_ff @(posedge clk or negedge rst_n) begin
-  if (!rst_n) pc_q <= '0;
-  else if (start)  pc_q <= pc;      // pc presented to IMEM this cycle
+	if (!Rst_n) pc_q <= '0;
+	else if (Start)  pc_q <= pc;      // pc presented to IMEM this cycle
 end
 
 // Build IF/ID input packet (aligned)
-assign if_id_d.pc    = pc_q;
-assign if_id_payld_nxt.instr = imem_data;
+assign if_payld_nxt.pc    = pc_q;
+assign if_payld_nxt.instr = Imem_Data;
 	
 pipe_reg #(.T(If_Id_t)) u_if_id (
   .clk     (clk),
@@ -61,9 +61,9 @@ pipe_reg #(.T(If_Id_t)) u_if_id (
   .en      (start),
   .stall   ('0),   // 0 for now
   .flush   ('0),   // 0 for now
-  .d       (if_id_payld_nxt),
-  .q       (if_id_payld),
-  .valid_q (if_id_valid)
+  .d       (if_payld_nxt),
+  .q       (If_Payld),
+  .valid_q (If_Valid)
 );
 //==================================================//
 
