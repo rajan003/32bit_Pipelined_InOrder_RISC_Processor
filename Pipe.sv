4925544aec32e9a27a46816c2fdc9f06c223539c
Anurag Singh <anuragsi003@gmail.com>
2026-01-06 17:52:16 +0100

    Update Pipe.sv

--- Pipe.sv
+++ Pipe.sv
@@ -5,36 +5,32 @@
 //------------------------------------------------------------------------------
 // Generic pipeline register for ANY packed type T
 //------------------------------------------------------------------------------
-module pipe_reg #(
-  parameter type T = logic [31:0]   // override with your struct type
+module pipe #(
+  parameter WIDTH =32)   // override with your struct type
 )(
   input  logic clk,
   input  logic rst_n,   // active-low
   input  logic en,      // global enable/start
   input  logic stall,   // hold
   input  logic flush,   // bubble
-  input  T     d,
-  output T     q,
+  input  logic [WIDTH-1:0]  d,
+  output logic [WIDTH-1:0]  q,
   output logic valid_q
 );
 
-  function automatic T bubble();
-    T t;
-    t = '0;      // works for packed structs/vectors
-    return t;
-  endfunction
+
 
   always_ff @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-      q       <= bubble();
+      q       <= '0;
       valid_q <= 1'b0;
     end
     else if (!en) begin
-      q       <= bubble();
+      q       <= '0;
       valid_q <= 1'b0;
     end
     else if (flush) begin
-      q       <= bubble();
+      q       <= '0;
       valid_q <= 1'b0;
     end
     else if (!stall) begin
