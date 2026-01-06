fcf9127a176271e3b29354cabc7bc23f1df7b8ec
Anurag Singh <anuragsi003@gmail.com>
2026-01-06 17:24:19 +0100

    Update Ex_Stage.sv

--- Ex_Stage.sv
+++ Ex_Stage.sv
@@ -2,44 +2,36 @@
 ///Package Importt/
 `include "cpu_pkg.sv"
 module Ex_Stage (
-                input logic clk,
-  				input logic rst,
+                input logic Clk,
+  				input logic Rst,
   
-  				input logic start,
+  				input logic Start,
   		  //Of Stage Output
-			  	output Of_Ex_t Of_Ex_q,
-  				output logic Of_Ex_Valid
+			  	output Of_Ex_t Of_Payld,
+  				output logic Of_Valid,
 
        //Ex Stage Output
-			  	output Ex_Ma_t ex_ma_q,
-  				output logic ex_ma_valid,  
+			  	output Ex_Ma_t Ex_Payld,
+  				output logic Ex_Valid,  
 
 	   ///Branch Control To IF Stage
-	            output logic isBranchTaken,
+	            output logic IsBranchTaken,
 	            output logic [31:0] BranchPC
 ) ;
 
-
 	
-///---------------------------------------//
-//-----------Execute Unit-----------------//
-//----------------------------------------//
-//Execution of instruction are 2 types
-
 //-----------------------------------------------------------------------------//
 //--------------Type-1 : Execution of Branched Instruction--------------------//
 //----------------------------------------------------------------------------//
-
-logic [31:0] BranchPC;
-flag_t flags, flags;
+flag_t flags, flags_q;
 //Now on What condition you want PC to move to Branched instruction 
 /// type-1 branch inst: Unconditional Brnahc ( b , call, ret )
 /// type-2, Conditional branch : beq, bne >> they depend on Last instrcution (CMP) result i.e flag 
-    always@* begin 
-      isBranchTaken = Of_Ex_q.ctrl.isUBranch | (Of_Ex_q.ctrl.isBgt & flags.GT) | (Of_Ex_q.ctrl.isBeq & flags.ET) ;  /// (Type-1 OR Type-2) 
+ always@* begin 
+	 isBranchTaken = Of_Payld.ctrl.isUBranch | (Of_Payld.ctrl.isBgt & flags_q.GT) | (Of_Payld.ctrl.isBeq & flags_q.ET) ;  /// (Type-1 OR Type-2) 
  
-       BranchPC = Of_Ex_q.BranchPC ; //  BranchPC is already calculated in OF stage 
-    end 
+     BranchPC = Of_Payld.BranchPC ; //  BranchPC is already calculated in OF stage 
+ end 
   
 
 //-----------------------------------------------------------------------------//
@@ -47,10 +39,11 @@ flag_t flags, flags;
 //----------------------------------------------------------------------------//
 aluctrl_t aluSignal ; /// ALU control signals
 logic [31:0] op1,op2;
+logic [31:0] alu_result;
 always_comb begin 
-	op1 = Of_Ex_q.op1;
-	op2= Of_Ex_q.op2;
-	aluSignal = Of_Ex_q.ctrl.alu_ctrl
+	op1 = Of_Payld.op1;
+	op2=  Of_Payld.op2;
+	aluSignal = Of_Payld.ctrl.alu_ctrl
 end 
   
 ALU  alu_unit (
@@ -80,11 +73,13 @@ ALU  alu_unit (
                     // logic ET ;
   // } flg;
 );
+ always@(posedeg Clk, negesge Rst)
+	if(!Rst) flag_q <= '0;
+	else if(Of_Valid && start) flag_q <= flag;
 
-// EX stage outputs (combinational)
+//EX Payld 
 Ex_Ma_t ex_ma_d;
-// MA stage inputs (registered)
-Ex_Ma_t ex_ma_q;
+
 // Valid bit
 logic ex_ma_valid;
 // Control
@@ -94,25 +89,24 @@ logic ex_ma_valid;
 always_comb begin
   ex_ma_d = '0;
 
-  ex_ma_d.pc         = of_ex_q.pc;        // PC of instruction
+  ex_ma_d.pc         = Of_Payld.pc;        // PC of instruction
   ex_ma_d.aluresult  = alu_result;        // ALU output
-  ex_ma_d.op2        = of_ex_q.op2;        // store data (rs2)
-  ex_ma_d.instr      = of_ex_q.instr;      // instruction bits
-  ex_ma_d.ctrl       = of_ex_q.ctrl;       // control bundle
+  ex_ma_d.op2        = Of_Payld.op2;        // store data (rs2)
+  ex_ma_d.instr      = Of_Payld.instr;      // instruction bits
+  ex_ma_d.ctrl       = Of_Payld.ctrl;       // control bundle
 end
 	
-pipe u_pipe_ma (
-  .clk      (clk),
-  .rst_n    (rst_n),
+pipe #(.WIDTH(32*5)) u_pipe_ma (
+  .clk      (Clk),
+  .rst_n    (Rst),
 
   .en       (start),        // global pipeline enable
   .stall    (1'b0),   // usually 0 initially
   .flush    (1'b0),   // asserted on branch taken
 
   .ex_ma_d  (ex_ma_d),      // from EX stage (combinational)
-  .ex_ma_q  (ex_ma_q),      // into MA stage (registered)
-
-  .valid_q  (ex_ma_valid)   // tells MA if this is a real instruction
+  .ex_ma_q  (Ex_Payld),      // into MA stage (registered)
+  .valid_q  (Ex_valid)   // tells MA if this is a real instruction
 );
 
 endmodule 
