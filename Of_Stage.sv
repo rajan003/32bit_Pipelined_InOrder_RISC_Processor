3fbb3b3404d6c4802fd9c09abcd6295d3b4d069c
Anurag Singh <anuragsi003@gmail.com>
2026-01-06 17:04:41 +0100

    Update Of_Stage.sv

--- Of_Stage.sv
+++ Of_Stage.sv
@@ -1,58 +1,59 @@
 //Package Importt/
 `include "cpu_pkg.sv"
 module Of_Stage (
-                input logic clk,
-  				input logic rst,
+                input logic Clk,
+  				input logic Rst,
+				input logic Start,
 			//IF Stage Output
 				input If_Of_t If_Payld,
-				input logic If_valid,
+				input logic If_Valid,
   
 
             /// Immediate bit output to Control Unit
-                output logic Cu_imm, /// immediate indication bi
-                output logic [4:0] Cu_opcode  //Opcode
-                input ctrl_unit_t Cu_out
+                output logic Cu_Imm, /// immediate indication bi
+ 				output logic [4:0] Cu_Opcode  //Opcode
+                input ctrl_unit_t Cu_Out
 
 			// GPR Reg Interface 
 			  //// Read Ports-0//////
-				output logic [3:0] rd_addr1,
-				input logic [31:0] rd_data1,
+				output logic [3:0] Rd_Addr1,
+				input logic [31:0] Rd_Data1,
 			  ///Read Port-1///
-				output logic [3:0] rd_addr2,
-				input logic [31:0] rd_data2
+				output logic [3:0] Rd_Addr2,
+				input logic [31:0] Rd_Data2
 			// Ex Stage 
-				output Of_Ex_t of_ex_q,
-  				output logic of_ex_valid
+				output Of_Ex_t Of_Payld,
+  				output logic Of_Valid
 		);
 
   
 //---------------------------------------------------------//
 //--------------- Decode: Register read and write----------//
 //---------------------------------------------------------//
-// Read Interface Control 
- assign Cu_opcode = If_Of[31:27] ;
-//=====>>IF Opcode to Control Unit===>Control Signal==//
-	
-  logic [3:0] ra_addr ; /// Return Address Register
-  logic [3:0] rd_addr1_int, rd_addr2_int ;
+  logic [3:0] Ra_Addr ; /// Return Address Register
+  logic [3:0] Rd_Addr1, Rd_Addr2 ;
   logic [31:0] op1, op2, op2_int ; /// Two Outputs from Register file.
-//------ Operand Generation for ALU----//
+  //------ Operand Generation for ALU----//
     // Format     Defition
 	// branch     register op (27-31) offset (1-27) op )  
 	// register   op (27-31) I (26) rd (22-25) rs1 (18-21) rs2 (14-17)
 	// immediate  op (27-31) I (26) rd (22-25) rs1 (18-21) imm (0-17)
-// op-> opcode, offset-> branch offset,  I-> immediate bit, rd -> destinaton register, rs1 -> source register 1, rs2 -> source register 2, imm -> immediate operand
+  // op-> opcode, offset-> branch offset,  I-> immediate bit, rd -> destinaton register, rs1 -> source register 1, rs2 -> source register 2, imm -> immediate operand
 
   always@* begin 
-		ra_addr ='0;
-	    rd_addr1_int='0;
-	    rd_addr2_int='0;
-	  if(If_vld) begin 
-      ra_addr = 4'b1111 ; // the 16th regitser in GPR is reserved for storing PC value
-  	  rd_addr1_int = Cu_out.isRet ? ra_addr : If_Payld.instr[21:18] ; ///  register Read Address Port-1// RA or RS1 always
-	  rd_addr2_int = Cu_out.isSt ? If_Payld.instr[25:22] : If_Payld.instr[17:14] ; /// Store instructure= RD , rest are Rs2 
-	  end 
-  end 
+		Ra_Addr ='0;
+	    Rd_Addr1='0;
+	    Rd_Addr2='0;
+	    Cu_Opcode = '0 ;
+	    Cu_Imm = '0;
+	  if(If_Valid) begin 
+	      Ra_Addr = 4'b1111 ; // the 16th regitser in GPR is reserved for storing PC value
+		  Cu_Imm = If_Payld.instr[26]; /// Immediate Bit from the instruction 
+		  Cu_Opcode = If_Payld.instr[31:27] ; /// Opcode for the Control Unit// //=====>>IF Opcode to Control Unit===>Control Signal==//
+	  	  Rd_Addr1 = Cu_Out.isRet ? Ra_Addr : If_Payld.instr[21:18] ; ///  register Read Address Port-1// RA or RS1 always
+		  Rd_Addr2 = Cu_Out.isSt ? If_Payld.instr[25:22] : If_Payld.instr[17:14] ; /// Store instructure= RD , rest are Rs2 
+	    end 
+   end 
 
 ///Calculaing the Immediate extension bits
 	logic [31:0] immx;
@@ -67,27 +68,14 @@ module Of_Stage (
 	
 	always_comb
 		begin 
-		 op1 = rd_data1;
-		 op2_int = rd_data2;
-		 op2 = Cu_out.isImmediate ? immx : op2_int; /// Is Instruction is immediate than Immediate Value otherwise it's an register Instrcution(rs2)
+		 op1 = Rd_Data1;
+		 op2_int = Rd_Data1;
+		 op2 = Cu_Out.isImmediate ? immx : op2_int; /// Is Instruction is immediate than Immediate Value otherwise it's an register Instrcution(rs2)
 		end 
 
-//=======Branch target========================//
-//Now on What condition you want PC to move to Branched instruction 
-/// type-1 branch inst: Unconditional Brnahc ( b , call, ret )
-/// type-2, Conditional branch : beq, bne >> they depend on Last instrcution (CMP) result i.e flag 
-	logic [31:0] BranchPC;
-	logic [31:0] BranchTarget ,BranchTarget_int ;
-    always@* begin 
-        //// Calculating the Branch Instruction Offset(nneded in both Conditional and Uncondiional branch instr except ret )
-		BranchTarget_int = If_Payld.instr[26:0] >> 2 ; // Shifted Offset , This is done to make it Word Addressing 
-        BranchTarget = If_Payld.pc + ({{5{BranchTarget_int[26]}} , BranchTarget_int[26:0]}); /// Branch Target = PC + Sign-Extension of Shifted Offset
-        BranchPC = Cu_out.isRet ? op1 : BranchTarget ; //  Is the Instrcution is retention type You will read the RA register for Last saved Instruction Address to pick up 
-    end 
 	
 //===============OF Pipeline==========================//
 Of_Ex_t of_ex_d, of_ex_q; /// OF pipeline Payload 
-logic  of_ex_valid;
 
 // OF stage (combinational build of packet)
 always_comb begin
@@ -99,19 +87,19 @@ always_comb begin
   of_ex_d.A            = op1;
   of_ex_d.B            = op2;
   of_ex_d.op2          = op2_int;      // imm/reg selected
-  of_ex_d.ctrl         = Cu_out;      // ctrl_unit_t
+  of_ex_d.ctrl         = Cu_Out;      // ctrl_unit_t
 end
 
 // OF/EX pipeline register
 pipe_of_ex u_of_ex (
-  .clk      (clk),
-  .rst_n    (rst_n),
-  .en       (start),        // your start/enable
+  .clk      (Clk),
+  .rst_n    (Rst),
+  .en       (Start),        // your start/enable
   .stall    ('0),   // hook later (0 for now)
   .flush    ('0),   // hook later (0 for now)
   .of_ex_d  (of_ex_d),
-  .of_ex_q  (of_ex_q),
-  .valid_q  (of_ex_valid)
+  .of_ex_q  (Of_Payld),
+  .valid_q  (Of_Valid)
 );
 
 
